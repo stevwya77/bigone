c173bf27cd448fff92a641f0e76f021dfc6cde04
Steph <45020090+stevwya77@users.noreply.github.com>
2026-03-14 03:36:04 -0400

    skeleton

--- main.go
+++ main.go
@@ -1,7 +1,110 @@
 package main
 
-import "fmt"
+// An example Bubble Tea server. This will put an ssh session into alt screen
+// and continually print up to date terminal information.
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+)
+
+const (
+	host = "localhost"
+	port = "3000"
+)
 
 func main() {
-	fmt.Println("foo bar")
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			activeterm.Middleware(), // Bubble Tea apps usually require a PTY.
+			logging.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+}
+
+/*-----------------------------------------------*/
+
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	s.Pty()
+	return initialModel(), []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+type model struct {
+	ti textinput.Model
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Focus()
+	ti.Placeholder = "John Appleseed"
+	ti.Width = 20
+
+	return model{
+		ti,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if val, ok := msg.(tea.KeyMsg); ok {
+		key := val.String()
+		if key == "ctrl+c" {
+			return m, tea.Quit
+		}
+		if key == "enter" {
+			// save to file first
+			os.WriteFile("output.log", []byte(m.ti.Value()), 0644)
+			return m, tea.Quit
+		}
+	}
+	var cmd tea.Cmd
+	m.ti, cmd = m.ti.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	output := fmt.Sprintf("What is your name?\n\n%v", m.ti.View())
+	return output
 }
